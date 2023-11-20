15b1f3aaf87b769b84c33170a9348374870c3953
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-11-20 21:02:17 +0200

    Remove the excess and supplement main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import { ChakraProvider, theme, ColorModeScript } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <BrowserRouter>
+        <Helmet>
+          <meta charSet="UTF-8"></meta>
+          <title>Telegram Statistics</title>
+          <meta name="keywords" content="Telegram Statistics, Telegram" />
+          <meta name="author" content="Anastasiia Mudra"></meta>
+        </Helmet>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
+);
